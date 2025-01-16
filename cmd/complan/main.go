56ed251dffde0a006c0b505cbd365bf3a4377bf5
Jlexender <aleksandrdmitryevich@gmail.com>
2025-01-16 22:40:06 +0300

    impl: corrected image export

--- cmd/complan/main.go
+++ cmd/complan/main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"Jlexender/complan/internal/mapping"
 	"Jlexender/complan/internal/image"
+	"Jlexender/complan/internal/mapping"
+	"fmt"
+	"math"
+	"math/cmplx"
 )
 
 func main() {
+
 	fmt.Println("Hello, world!")
 
-	set := mapping.GenerateRectangleSet(complex(-1, -1), complex(1, 1), 0.1)
+	set := mapping.GenerateRectangleSet(complex(0, 0), complex(10, math.Pi), 0.1)
 
-	err := image.PlotComplexNumbers(set, "input.png")
+
+	err := image.PlotComplexNumbers(set, "input.png", 5, 5, 5)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	mapping.TransformInPlace(set, func(z complex128) complex128 {
+		return cmplx.Exp(complex(real(z), imag(z)/2))
+	})
+
+	err = image.PlotComplexNumbers(set, "output.png", 5, 5, 5)
 
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 
 	fmt.Println("Done!")
-	
 }
--- internal/image/gen.go
+++ internal/image/gen.go
@@ -1,46 +1,58 @@
 package image
 
 import (
-	"image"
+    "image"
     "image/color"
-	"image/png"
-	"os"
+    "image/png"
+    "os"
 )
 
 // PlotComplexNumbers plots the given complex numbers on a complex plane.
-func PlotComplexNumbers(numbers []complex128, filename string) error {
-	const width, height = 800, 800
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	black := color.RGBA{0, 0, 0, 255}
-	white := color.RGBA{255, 255, 255, 255}
+func PlotComplexNumbers(numbers []complex128, filename string, radius int, maxX, maxY float64) error {
+    const width, height = 1600, 1600
+    img := image.NewRGBA(image.Rect(0, 0, width, height))
+    black := color.RGBA{0, 0, 0, 255}
+    white := color.RGBA{255, 255, 255, 255}
 
-	// Fill the image with white color
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			img.Set(x, y, white)
-		}
-	}
+    for x := 0; x < width; x++ {
+        for y := 0; y < height; y++ {
+            img.Set(x, y, white)
+        }
+    }
 
-	// Plot each complex number
-	for _, num := range numbers {
-		x := int(real(num)*float64(width)/2 + float64(width)/2)
-		y := int(imag(num)*float64(height)/2 + float64(height)/2)
-		if x >= 0 && x < width && y >= 0 && y < height {
-			img.Set(x, y, black)
-		}
-	}
+    generateAxis(img, width, height)
 
-	// Save the image to a file
-	file, err := os.Create(filename)
+    for _, num := range numbers {
+        x := int((real(num)/maxX + 1) * float64(width) / 2)
+        y := int((1 - imag(num)/maxY) * float64(height) / 2)
+        drawCircle(img, x, y, radius, black)
+    }
+
+    file, err := os.Create(filename)
 	if err != nil {
 		return err
-	}
-	defer file.Close()
+    }
+    defer file.Close()
 
-	err = png.Encode(file, img)
-	if err != nil {
-		return err
-	}
+    return png.Encode(file, img)
+}
 
-	return nil
+func generateAxis(img *image.RGBA, width, height int) {
+    black := color.RGBA{0, 0, 0, 255}
+    for x := 0; x < width; x++ {
+        img.Set(x, height/2, black)
+    }
+    for y := 0; y < height; y++ {
+        img.Set(width/2, y, black)
+    }
+}
+
+func drawCircle(img *image.RGBA, x0, y0, radius int, col color.Color) {
+    for y := -radius; y <= radius; y++ {
+        for x := -radius; x <= radius; x++ {
+            if x*x+y*y <= radius*radius {
+                img.Set(x0+x, y0+y, col)
+            }
+        }
+    }
 }
