f254d14adeec03e981b90a8569ed06dbe389bcc0
Jlexender <aleksandrdmitryevich@gmail.com>
2025-01-16 22:49:13 +0300

    base path to constants

--- cmd/complan/main.go
+++ cmd/complan/main.go
@@ -9,14 +9,16 @@ import (
 )
 
 func main() {
-
-	fmt.Println("Hello, world!")
+	const (
+		width, height = 800, 800
+		radius        = 3
+		limit         = 10
+		basePath      = "../../out/"
+	)
 
 	set := mapping.GenerateRectangleSet(complex(0, 0), complex(10, math.Pi), 0.1)
 
-
-	err := image.PlotComplexNumbers(set, "input.png", 5, 5, 5)
-
+	err := image.PlotComplexNumbers(set, basePath+"input.png", radius, limit, limit)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
@@ -25,8 +27,7 @@ func main() {
 		return cmplx.Exp(complex(real(z), imag(z)/2))
 	})
 
-	err = image.PlotComplexNumbers(set, "output.png", 5, 5, 5)
-
+	err = image.PlotComplexNumbers(set, basePath+"output.png", radius, limit, limit)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
